cef79c919cef87349996a45bbdc8a99235509b99
Goye1 <manuel.goyenechea@gmail.com>
2024-04-07 08:06:37 -0300

    back

--- src/containers/Formularios/Formularios.jsx
+++ src/containers/Formularios/Formularios.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Select, MenuItem } from '@mui/material';
 import './formulario.css';
 import SidenNav from '../../components/SideNav';
@@ -9,6 +9,24 @@ function Formularios() {
         { question: '', answers: [] }
     ]);
     const [selectedOption, setSelectedOption] = useState('');
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        const fetchPatients = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/medic/get-patients?doctorId=1');
+                if (!response.ok) {
+                    throw new Error('Error al obtener los pacientes');
+                }
+                const data = await response.json();
+                setPatients(data);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchPatients();
+    }, []);
 
     const addQuestion = () => {
         setQuestions([...questions, { question: '', answers: [] }]);
@@ -46,9 +64,41 @@ function Formularios() {
         setSelectedOption(event.target.value);
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(questions);
+
+        const qars = questions.map(question => ({
+            question: question.question,
+            possibleAnswers: question.answers
+        }));
+
+        const formData = {
+            qars,
+            doctorId: 1,
+            patientId: selectedOption
+        };
+
+        try {
+            const response = await fetch('http://localhost:8080/form/createForm', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            console.log(data);
+            window.location.reload();
+
+        } catch (error) {
+            console.error('There was a problem with your fetch operation:', error);
+           
+        }
     };
 
     return (
@@ -61,18 +111,20 @@ function Formularios() {
                         <div className="buttons-container">
                             <button type="button" onClick={addQuestion}>Agregar Pregunta</button>
                             <Select
-                    value={selectedOption}
-                    onChange={handleDropdownChange}
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                >
-                    <MenuItem value="" disabled>
-                        <em>Selecciona una paciente</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Paciente 1</MenuItem>
-                    <MenuItem value={20}>Paciente 2</MenuItem>
-                    <MenuItem value={30}>Paciente 3</MenuItem>
-                </Select>
+                                value={selectedOption}
+                                onChange={handleDropdownChange}
+                                displayEmpty
+                                inputProps={{ 'aria-label': 'Without label' }}
+                            >
+                                <MenuItem value="" disabled>
+                                    <em>Selecciona un paciente</em>
+                                </MenuItem>
+                                {patients.map(patient => (
+                                    <MenuItem key={patient.id} value={patient.id}>
+                                        {patient.name} {patient.surname}
+                                    </MenuItem>
+                                ))}
+                            </Select>
                             <button type="submit">Enviar</button>
                         </div>
                         <div className="questions-container">
@@ -124,8 +176,6 @@ const styles = {
       bgcolor: 'neutral.light',
       height: 'calc(100% -64px)'
     }
-  }
-  
+}
+
 export default Formularios;
-
-
